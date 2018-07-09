0be530a940ba3caf844201fd65b15f070ee74eb3
danielsalgadop <daniel.salgado.p@gmail.com>
2018-07-09 02:26:25 +0200

    done CacheService, but not in correct place

--- src/FilmApp/Application/CommandHandler/Film/ReadFilmCommandHandler.php
+++ src/FilmApp/Application/CommandHandler/Film/ReadFilmCommandHandler.php
@@ -4,19 +4,26 @@ namespace FilmApp\Application\CommandHandler\Film;
 use FilmApp\Domain\Film;
 use FilmApp\Domain\Repository\FilmRepository;
 use FilmApp\Domain\Repository\ActorRepository;
+use FilmBundle\Service\CacheService;
 
 
 class ReadFilmCommandHandler
 {
     private $filmRepository;
+    private $cache;
 
-    public function __construct(FilmRepository $filmRepository)
+    public function __construct(FilmRepository $filmRepository, CacheService $cache)
     {
         $this->filmRepository = $filmRepository;
+        $this->cache = $cache;
     }
 
     public function handle(int $id)
     {
-        return $this->filmRepository->findFilmByIdOrError($id);
+        if($this->cache->get($id) == null){
+            $film = $this->filmRepository->findFilmByIdOrError($id);
+            $this->cache->set($film);
+        }
+        return $this->cache->get($id);
     }
 }
--- src/FilmBundle/Service/CacheService.php
+++ src/FilmBundle/Service/CacheService.php
@@ -1,42 +1,54 @@
 <?php
 
 namespace  FilmBundle\Service;
+use Symfony\Component\Filesystem\Filesystem;
 
 class CacheService
 {
     const SEPARATOR = ':';
     private $cache;
-    private $persistence_path;
+    private $persistence_file;
 
     public function __construct($persistence_path){
-        $this->persistence_path = $persistence_path;
+        $this->persistence_file = $persistence_path.'/cache';
+
+        $fileSystem = new Filesystem();
+        $fileSystem->mkdir($persistence_path, 0777);
+        $fileSystem->touch($this->persistence_file, 0777);
         $this->persistentData2Cache();
     }
 
-    public function get($id, $value)
+    public function get(int $id)
     {
         if(array_key_exists($id, $this->cache)){
             return $this->cache[$id];
         }
+        return null;
     }
 
-    public function set($id,$object){
-        $this->cache[$id] = $object;
+    public function set($film){
+        $this->cache[$film->getId()] = $film;
         $this->persist();
-
     }
+
     private function persistentData2Cache(){
-        foreach(file($this->persistence_path as $line)){
-            list($id, $s_object) = explode(self::SEPARATOR,$line,1);
-                $obect = unserialize($s_object);
-                $this->cache[$id] = $s_object;
+
+        $arr = file($this->persistence_file);
+        $this->cache = [];
+
+        if(count($arr) > 0){
+            foreach($arr as $line){
+                list($id, $s_film) = explode(self::SEPARATOR,$line,2);
+                    $film = unserialize($s_film);
+                    $this->cache[$id] = $film;
+            }
         }
     }
 
     private function persist()
     {
-        foreach($this->cache as $id => $object){
-            file_put_contents($this->persistence_path, $id.self::SEPARATOR.serialize($object)."\n", FILE_APPEND);
+        foreach($this->cache as $id => $film){
+            file_put_contents($this->persistence_file, $id.self::SEPARATOR.serialize($film)."\n", FILE_APPEND);
         }
     }
     public function clean()
